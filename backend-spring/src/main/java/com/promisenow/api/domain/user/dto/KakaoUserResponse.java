f62176031ef4f8b0ea697995aeba8702c6ac5ee4
DESKTOP-DLOPF19\SSAFY <12000ha@naver.com>
2025-07-27 16:33:09 +0900

    feat: 카카오 토큰과 유저 정보 응답을 위한 DTO 추가

---
+++ backend-spring/src/main/java/com/promisenow/api/domain/user/dto/KakaoUserResponse.java
@@ -0,0 +1,18 @@
+package com.promisenow.api.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoUserResponse {
+
+    private Long id; // 카카오 사용자 고유 ID
+    private Map<String, String> properties; // 가입 날짜
+}
