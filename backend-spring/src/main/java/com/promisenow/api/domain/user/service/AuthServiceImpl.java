743d9040c37c024d8f8a091d45fe30361c352bd9
DESKTOP-DLOPF19\SSAFY <12000ha@naver.com>
2025-07-27 16:47:30 +0900

    feat: 카카오 로그인, JWT 발급 구현

---
+++ backend-spring/src/main/java/com/promisenow/api/domain/user/service/AuthServiceImpl.java
@@ -0,0 +1,33 @@
+package com.promisenow.api.domain.user.service;
+
+import com.promisenow.api.domain.user.dto.KakaoUserResponse;
+import com.promisenow.api.domain.user.entity.User;
+import com.promisenow.api.global.jwt.JwtTokenProvider;
+import com.promisenow.api.global.oauth.KakaoOAuthClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final KakaoOAuthClient kakaoOAuthClient;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public String handleKakaoLogin(String code) {
+
+        // 카카오 액세스 토큰 요청
+        String accessToken = kakaoOAuthClient.requestAccessToken(code);
+
+        // 카카오 유저 정보 요청
+        KakaoUserResponse kakaoUser = kakaoOAuthClient.requestUserInfo(accessToken);
+
+        // DB에 해당 사용자 존재 여부 확인 및 생성
+        User user = userService.findOrCreateUser(kakaoUser.getId());
+
+        // 카카오 고유 ID로 JWT 토큰 생성 후 반환
+        return jwtTokenProvider.generateToken(user.getUserId());
+    }
+}
