bce118730d8433aa332659dc24a4321d6df05005
jiyunkim <jiyunkim831@gmail.com>
2025-07-29 15:29:10 +0900

    feat: 상단 하단 화살표, ai 아이콘 추가

--- frontend/src/types/icons.type.ts
+++ frontend/src/types/icons.type.ts
@@ -1,13 +1,15 @@
 import type { IconType as ReactIconType } from 'react-icons';
-import { BsFillChatFill } from 'react-icons/bs';
+import { BsFillChatFill, BsStars } from 'react-icons/bs';
 import { FiCalendar, FiMapPin, FiPhoneCall, FiSettings, FiTarget } from 'react-icons/fi';
 import { IoMdTime, IoMdRepeat } from 'react-icons/io';
 import { IoLocationOutline, IoPersonOutline } from 'react-icons/io5';
-import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
+import { SlArrowLeft, SlArrowRight, SlArrowUp, SlArrowDown } from 'react-icons/sl';
 
 export type IconType =
   | 'left'
   | 'right'
+  | 'up'
+  | 'down'
   | 'person'
   | 'roulette'
   | 'calendar'
@@ -17,11 +19,14 @@ export type IconType =
   | 'chat'
   | 'time'
   | 'location'
-  | 'repeat';
+  | 'repeat'
+  | 'ai';
 
 export const iconMap: Record<IconType, ReactIconType> = {
   left: SlArrowLeft,
   right: SlArrowRight,
+  up: SlArrowUp,
+  down: SlArrowDown,
   person: IoPersonOutline,
   roulette: FiTarget,
   calendar: FiCalendar,
@@ -32,4 +37,5 @@ export const iconMap: Record<IconType, ReactIconType> = {
   time: IoMdTime,
   location: IoLocationOutline,
   repeat: IoMdRepeat,
+  ai: BsStars,
 };
