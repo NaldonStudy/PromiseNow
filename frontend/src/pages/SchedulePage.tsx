94a6f298163a43c566c5d9a39a3a45d8a28e8e57
jiyunkim <jiyunkim831@gmail.com>
2025-07-29 15:28:36 +0900

    feat: 추천 일정 컴포넌트 추가 및 일정 페이지에 추가

--- frontend/src/pages/SchedulePage.tsx
+++ frontend/src/pages/SchedulePage.tsx
@@ -1,11 +1,13 @@
 import RoomLayout from '../components/layout/RoomLayout';
 import Calendar from '../features/calendar/components/Calendar';
+import ScheduleRecommendation from '../features/scheduleRecommendation/components/ScheduleRecommendation';
 
 const SchedulePage = () => {
   return (
     <RoomLayout>
       <div className="p-5">
         <Calendar />
+        <ScheduleRecommendation />
       </div>
     </RoomLayout>
   );
