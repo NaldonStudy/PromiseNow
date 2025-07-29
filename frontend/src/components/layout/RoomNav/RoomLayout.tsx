e316caf16da0ef6d1d1393be51c9ce51197abe56
tooxeong <sujeong3724@gmail.com>
2025-07-29 14:12:58 +0900

    refactor: RoomHeader 및 RoomLayout 컴포넌트 디렉토리 구조 개선

--- frontend/src/components/layout/RoomNav/RoomLayout.tsx
+++ frontend/src/components/layout/RoomNav/RoomLayout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
-import RoomHeader from './RoomHeader';
-import RoomNav from './roomNav/RoomNav';
+import RoomHeader from '../roomHeader/RoomHeader';
+import RoomNav from './RoomNav';
 
 interface Props {
   children: ReactNode;
