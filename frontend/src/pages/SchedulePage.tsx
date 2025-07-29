fb919b1dd06ea3db9ab4dc847a0748e3a931b6a7
tooxeong <sujeong3724@gmail.com>
2025-07-29 14:14:12 +0900

    refactor: 페이지 컴포넌트 내 레이아웃 import 경로 수정

--- frontend/src/pages/SchedulePage.tsx
+++ frontend/src/pages/SchedulePage.tsx
@@ -1,10 +1,12 @@
-import RoomLayout from '../components/layout/RoomLayout';
+import RoomLayout from '../components/layout/roomNav/RoomLayout';
 import Calendar from '../features/calendar/components/Calendar';
+import ConfirmedAppointment from './../features/appointment/ConfirmedAppointment';
 
 const SchedulePage = () => {
   return (
     <RoomLayout>
       <div className="p-5">
+        <ConfirmedAppointment />
         <Calendar />
       </div>
     </RoomLayout>
