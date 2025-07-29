b58e2311cd00d536865b4d1a79c8f1f1e9788b87
DESKTOP-DLOPF19\SSAFY <12000ha@naver.com>
2025-07-29 12:21:45 +0900

    feat: 사용자 로그인 정보 확인하는 임시 메서드 추가

--- backend-spring/src/main/java/com/promisenow/api/domain/user/controller/UserController.java
+++ backend-spring/src/main/java/com/promisenow/api/domain/user/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.promisenow.api.domain.user.controller;
 
+import com.promisenow.api.global.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-    
-    
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/me")
+    public ResponseEntity<Long> getMyInfo(HttpServletRequest request) {
+        String token = jwtTokenProvider.resolveToken(request);
+        Long userId = jwtTokenProvider.getUserId(token);
+        return ResponseEntity.ok(userId);
+    }
 
 } 
