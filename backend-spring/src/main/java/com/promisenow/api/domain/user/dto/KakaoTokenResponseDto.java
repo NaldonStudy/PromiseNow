3526fbf31f0d659a904f64b3d633acc6281f94f4
SeungJun Yoo <12000ha@naver.com>
2025-07-31 14:51:29 +0900

    style: lombok import 수정

--- backend-spring/src/main/java/com/promisenow/api/domain/user/dto/KakaoTokenResponseDto.java
+++ backend-spring/src/main/java/com/promisenow/api/domain/user/dto/KakaoTokenResponseDto.java
@@ -1,7 +1,10 @@
 package com.promisenow.api.domain.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
