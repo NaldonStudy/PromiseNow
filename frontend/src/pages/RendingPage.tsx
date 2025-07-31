beeeb035fd24d7313ffb03ca3517d244e882452a
조민기 <nstgic3@gmail.com>
2025-07-31 09:28:48 +0900

    S13 p11 b107 150 room room

--- frontend/src/pages/RendingPage.tsx
+++ frontend/src/pages/RendingPage.tsx
@@ -9,7 +9,7 @@ const RendingPage = () => {
       </span>
       <span className="text-sm text-primary">지금 PromissNow를 시작해보세요!</span>
       <button className="flex flex-row bg-yellow-300 font-semibold px-5 py-2 text-sm hover:bg-yellow-400 focus:outline-none">
-        <Icon type="chat" size={15} color="black" />
+        <Icon type="kakaotalk" size={18} color="black" />
         <span className="px-10">카카오로 시작하기</span>
       </button>
     </div>
