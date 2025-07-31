a322b6a148839774b37674c7901db275f07fd885
DESKTOP-DLOPF19\SSAFY <12000ha@naver.com>
2025-07-31 09:14:13 +0900

    style: 필요없는 라이브러리 제거

--- backend-spring/src/main/java/com/promisenow/api/domain/user/controller/AuthController.java
+++ backend-spring/src/main/java/com/promisenow/api/domain/user/controller/AuthController.java
@@ -8,7 +8,6 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Map;
 
 // @CrossOrigin(origins = "*")
 @RestController
