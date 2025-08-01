8601fa8b438b8099dcde46fac6cd8bd411c8f885
jiyunkim <jiyunkim831@gmail.com>
2025-08-01 17:54:49 +0900

    feat: MapView 컴포넌트 추가

--- frontend/src/pages/templates/LocationTemplate.tsx
+++ frontend/src/pages/templates/LocationTemplate.tsx
@@ -1,10 +1,12 @@
 import RoomLayout from '../../components/layout/RoomLayout';
 import ArrivalRanking from '../../features/arrivalRanking/components/ArrivalRanking';
+import MapView from '../../features/map/components/MapView';
 
 const LocationTemplate = () => {
   return (
     <RoomLayout>
-      <div className="bg-gray-dark h-full relative overflow-hidden">
+      <div className="h-full relative overflow-hidden">
+        <MapView />
         <div className="absolute bottom-0 w-full">
           <ArrivalRanking />
         </div>
