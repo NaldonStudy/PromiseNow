62779c33db80ae445d2f8ec574d48e8b1bdaa82c
jiyunkim <jiyunkim831@gmail.com>
2025-08-04 17:21:03 +0900

    style: 참여 코드에 복사 아이콘 추가

--- frontend/src/components/layout/roomHeader/CopyCode.tsx
+++ frontend/src/components/layout/roomHeader/CopyCode.tsx
@@ -1,7 +1,9 @@
+import Icon from '../../ui/Icon';
+
 const CopyCode = () => {
   return (
-    <div className="text-primary px-3 py-0.5 font-bold text-xs place-content-center border border-primary rounded-full">
-      참여 코드
+    <div className="flex justify-center items-center gap-1 text-primary px-3 py-0.5 font-bold text-xs border border-primary rounded-full">
+      참여 코드 <Icon type="copy" size={14} color="text-primary" />
     </div>
   );
 };
