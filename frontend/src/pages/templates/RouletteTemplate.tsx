c2c4e0d76d2e0df33dcda270ce6b177b0a64f1f4
jiyunkim <jiyunkim831@gmail.com>
2025-08-04 11:05:26 +0900

    feat: 룰렛 컴포넌트 추가

--- frontend/src/pages/templates/RouletteTemplate.tsx
+++ frontend/src/pages/templates/RouletteTemplate.tsx
@@ -1,9 +1,14 @@
 import RoomLayout from '../../components/layout/RoomLayout';
+import RouletteForm from '../../features/roulette/components/RouletteForm';
+import RouletteWheel from '../../features/roulette/components/RouletteWheel';
 
 const RouletteTemplate = () => {
   return (
     <RoomLayout>
-      <h1>룰렛 페이지</h1>
+      <div className="p-10 flex flex-col gap-5">
+        <RouletteForm />
+        <RouletteWheel />
+      </div>
     </RoomLayout>
   );
 };
