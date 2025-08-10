8b4694d36fb2d278544c15b1132141772761483e
tooxeong <sujeong3724@gmail.com>
2025-08-10 19:16:39 +0900

    feat: 카카오 로그인 기능 연동 - 카카오 OAuth 인증 플로우 구현 - 로그인 버튼 컴포넌트를 별도 파일로 분리 - RandingTemplate에 카카오 로그인 버튼 및 연동 로직 적용

--- frontend/src/pages/templates/RandingTemplate.tsx
+++ frontend/src/pages/templates/RandingTemplate.tsx
@@ -3,26 +3,24 @@ import { useUserStore } from '../../stores/user.store';
 
 import landingImg from '../../assets/images/landing.jpg';
 import PwaInstallButton from '../../components/PwaInstallButton';
-import Icon from '../../components/ui/Icon';
+import Kakao from '../../features/login/components/Kakao';
+import ManagerBtn from './../../features/login/components/ManagerBtn';
+
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 const RandingTemplate = () => {
   const navigate = useNavigate();
   const { setUser } = useUserStore();
 
-  // 임시 로그인
   const handleKakaoLogin = () => {
-    setUser(1);
-    navigate('/home');
+    window.location.href = `${API_BASE}/oauth2/authorization/kakao`;
   };
 
-  // 관리자용 ( 추후 삭제 예정 )
-  // 유저 1
   const handleKakaoLogin_1 = () => {
     setUser(1);
     navigate('/home');
   };
 
-  // 유저 2
   const handleKakaoLogin_2 = () => {
     setUser(2);
     navigate('/home');
@@ -35,27 +33,9 @@ const RandingTemplate = () => {
         약속부터 만남까지 한 화면에서 {<br />} 함께하는 즐거움
       </span>
       <span className="text-sm text-primary">지금 PromissNow를 시작해보세요!</span>
-
       <img src={landingImg} alt="landing img" className="max-w-full w-64 h-auto" />
-
-      {/* 로그인 버튼 */}
-      <button
-        className="flex flex-row bg-yellow-300 font-semibold px-5 py-2 text-sm hover:bg-yellow-400 focus:outline-none"
-        onClick={handleKakaoLogin}
-      >
-        <Icon type="kakaotalk" size={18} color="black" />
-        <span className="px-10">카카오로 시작하기</span>
-      </button>
-
-      {/* 관리자용 버튼 ( 추후 삭제 예정 ) */}
-      <div className="flex flex-row gap-10">
-        <button className="bg-blue-300 w-20 h-10" onClick={handleKakaoLogin_1}>
-          유저 1
-        </button>
-        <button className="bg-green-300 w-20 h-10" onClick={handleKakaoLogin_2}>
-          유저 2
-        </button>
-      </div>
+      <Kakao onLogin={handleKakaoLogin} />
+      <ManagerBtn onLogin1={handleKakaoLogin_1} onLogin2={handleKakaoLogin_2} />
       <PwaInstallButton />
     </div>
   );
