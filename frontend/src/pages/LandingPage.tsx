b2793625d5c18ada364eb7c3977b0069a190e275
jiyunkim <jiyunkim831@gmail.com>
2025-08-11 15:45:33 +0900

    refactor: LandingPage 컴포넌트의 사용자 인증 로직 추가

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserStore } from '../stores/user.store';
+
 import LandingTemplate from './templates/LandingTemplate';
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+  const { userId, isAuthenticated } = useUserStore();
+
+  useEffect(() => {
+    if (isAuthenticated && userId !== -1) {
+      navigate('/home');
+    }
+  }, [isAuthenticated, userId, navigate]);
+
   return <LandingTemplate />;
 };
 
