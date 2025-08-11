f50cb711a42a6e88ad1e0fe336eb50e0e34519bb
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-08-11 09:42:08 +0900

    feat: 웹소켓 관련 설정 수정

--- backend-spring/src/main/java/com/promisenow/api/config/WebSocketConfig.java
+++ backend-spring/src/main/java/com/promisenow/api/config/WebSocketConfig.java
@@ -19,9 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-leaderboard")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
-        //테스트용
+        
         registry.addEndpoint("/ws-leaderboard-native")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
