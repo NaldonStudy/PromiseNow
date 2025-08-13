5ece22505a106d78abfaedfea52edc881bda213a
jiyunkim <jiyunkim831@gmail.com>
2025-08-13 15:31:57 +0900

    fix: 비디오 온오프가 제대로 되지 않는 오류 수정

--- frontend/src/features/callScreen/dummy.ts
+++
@@ -1,7 +0,0 @@
-export const dummyCallUsers = [
-  { id: '1', name: '홍길동', isOnline: true, isMuted: false, videoStream: null },
-  { id: '2', name: '김철수', isOnline: true, isMuted: true, videoStream: null },
-  { id: '3', name: '박영희', isOnline: true, isMuted: false, videoStream: null },
-  { id: '4', name: '이민수', isOnline: true, isMuted: false, videoStream: null },
-  { id: '5', name: '최지우', isOnline: false, isMuted: true, videoStream: null },
-];
