baf8143a71bb0bbdad4917feb250f1bb6290dbe0
Mingi Cho <81455273+ChoMinGi@users.noreply.github.com>
2025-08-14 15:23:53 +0900

    feat: 거리와 속도 표시를 소수점 1자리로 포맷팅

--- frontend/src/features/arrivalRanking/components/ArrivalInfo.tsx
+++ frontend/src/features/arrivalRanking/components/ArrivalInfo.tsx
@@ -10,11 +10,11 @@ const ArrivalInfo = ({ distance, speed }: Props) => {
     <>
       <div className="text-[0.7rem] text-text-dark flex gap-2">
         <div className="flex items-center gap-1">
-          <Icon type="flag" size={10} color="text-text-dark" /> {distance} km 이내
+          <Icon type="flag" size={10} color="text-text-dark" /> {distance?.toFixed(1) || '0.0'} km 이내
         </div>
         <div className="flex items-center gap-1">
           <Icon type="speed" size={10} color="text-text-dark" />
-          {speed} km/h
+          {speed?.toFixed(1) || '0.0'} km/h
         </div>
       </div>
     </>
