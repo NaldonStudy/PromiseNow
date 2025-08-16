5a3d79026405ab9ef71d4ce1ad056d7816963248
jiyunkim <jiyunkim831@gmail.com>
2025-08-16 22:55:01 +0900

    fix: 모바일 레이아웃 오류 수정

--- frontend/src/components/layout/RoomLayout.tsx
+++ frontend/src/components/layout/RoomLayout.tsx
@@ -12,22 +12,26 @@ interface Props {
 const RoomLayout = ({ children }: Props) => {
   const { id } = useParams<{ id: string }>();
   const parsedRoomId = Number(id);
-  
+
   const { data: roomInfo } = useRoomTitleCode(parsedRoomId);
   const { data: users } = useUsersInRoom(parsedRoomId);
 
   return (
     <div className="flex justify-center bg-gray-100 min-h-screen">
       <div className="w-full max-w-mobile h-screen flex flex-col overflow-hidden bg-white relative">
-        <header className="fixed top-0 left-1/2 -translate-x-1/2 z-20 w-full max-w-mobile">
+        <header className="fixed top-0 left-1/2 -translate-x-1/2 z-20 w-full max-w-mobile h-14 pt-[env(safe-area-inset-top)]">
           <RoomHeader title={roomInfo?.roomTitle} inviteCode={roomInfo?.inviteCode} users={users} />
         </header>
 
-        <main className="relative flex-1 overflow-y-auto mt-[60px] mb-[70px] hide-scrollbar">
+        <main
+          className="relative flex-1 overflow-y-auto 
+             pt-[calc(56px+env(safe-area-inset-top))]
+             pb-[calc(70px+env(safe-area-inset-bottom))] hide-scrollbar"
+        >
           {children}
         </main>
 
-        <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 z-20 w-full max-w-mobile">
+        <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 z-20 w-full max-w-mobile h-[70px] pb-[env(safe-area-inset-bottom)]">
           <RoomNav />
         </footer>
       </div>
